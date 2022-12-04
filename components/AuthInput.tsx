8276ab5f2404e5df4e7412562f4a5ea5027680ad
Andrey <teryaev.2004@mail.ru>
2022-12-04 17:41:11 +0300

    style-form-auth-login

--- components/AuthInput.tsx
+++ components/AuthInput.tsx
@@ -4,9 +4,11 @@ interface IAuthInput {
   onChange: React.ChangeEventHandler<HTMLInputElement>, 
   value: string;
   title: string;
+  isPassword?: boolean;
+  type?: string;
 }
 
-const AuthInput: FC<IAuthInput> = ({onChange, value, title}) => {
+const AuthInput: FC<IAuthInput> = ({onChange, value, title, isPassword, type}) => {
   const [isValue, setIsValue] = React.useState<boolean>(false);
   const [isFocus, setIsFocus] = React.useState<boolean>(false);
 
@@ -27,7 +29,7 @@ const AuthInput: FC<IAuthInput> = ({onChange, value, title}) => {
       <div className={`input-wrapper relative w-[258px] min-h-[38px] rounded-[3px] bg-[#fafafa] overflow-hidden ${isValue && "active"} ${isFocus && "focus"}`}>
         <input
           className="input-field absolute z-[1] w-[100%] bg-transparent"
-          type="text"
+          type={type ? type : "text"}
           onChange={onChange}
           value={value}
           onFocus={focusHandler}
--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -20,12 +20,26 @@ const login: FC = () => {
         <Image className="absolute right-[30px] bottom-[60px]" src="/logTelBg.png" alt="telBg" width="230" height="460" />
       </div>
       <div>
-        <form className="bg-[#fff] flex flex-col items-center pb-4 px-[40px]">
+        <form className="auth-form bg-[#fff] flex flex-col items-center pb-5 px-[40px] rounded-[1px]">
           <Image src="/instaIcon.svg" alt="instagram" width="130" height="50" />
           <div className="flex flex-col gap-3">
             <AuthInput onChange={changeUserHandler} value={userValue} title="Телефон, имя пользователя или эл. адрес" />
-            <AuthInput onChange={changePasswordHandler} value={passwordValue} title="Пароль" />
+            <AuthInput onChange={changePasswordHandler} value={passwordValue} title="Пароль" isPassword={true} type="password" />
           </div>
+          <button className="w-[100%] min-h-[30px] py-[5px] px-[9px] bg-[#0095f6] rounded-[3px] mt-3 text-white text-[14px] font-semibold" type="submit">Войти</button>
+
+        <div className="relative text-center uppercase mt-4 text-[#868585] z-[3] w-[100%]">
+          <span className="bg-[#fff] relative p-1 z-[100] text-[13px] font-semibold">Или</span>
+          <div className="auth-line"></div>
+        </div>
+
+        <a className="mt-5 flex gap-2 items-center text-[#385185] font-semibold" href="#">
+          <Image src="/facebook.svg" alt="facebook" width="16" height="16" />
+          <span>Войти через Facebook</span>
+        </a>
+
+        <a className="text-[#00376B] text-[12px] mt-3" href="#">Забыли пароль?</a>
+
         </form>
         <div></div>
         <div></div>
